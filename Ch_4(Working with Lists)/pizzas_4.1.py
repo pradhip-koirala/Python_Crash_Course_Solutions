bb33d47be01f80ebbbdf9d1563ea4d9771a9eead
pradhip-koirala <pradhipkoirala2962023@gmail.com>
2025-12-03 20:32:14 +0530

    add comment to explain list

--- Ch_4(Working with Lists)/pizzas_4.1.py
+++ Ch_4(Working with Lists)/pizzas_4.1.py
@@ -1,3 +1,5 @@
+#list is collection of elements stored inside square bracket.
+
 pizzas = ['Neapolitan Pizza','Chicago' 
 'New York',
 'Sicilian',
@@ -5,7 +7,10 @@ pizzas = ['Neapolitan Pizza','Chicago'
 'California', 
 'Detroit']
 
+#accessing list elements using for loop
+#in here 'pizza' represents from list of pizzas take the first elements and print the desired output and like wise.
 for pizza in pizzas:
+
     print("I like",pizza,"Pizza.")
 
 print("I really like ",pizzas[2],"Pizzas")
